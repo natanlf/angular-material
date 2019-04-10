cfd6651162b94fcbd05c4cefdfc4502971bbf524
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-04-10 17:59:19 -0300

    Drag List

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, NgModule } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,21 @@ import { Component, OnInit, NgModule } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   showFiller = false;
-  panelOpenState = false;
+
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
 
   constructor() { }
 
