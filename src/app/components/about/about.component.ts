02e1ab27eb8e0a1d2549b026c81cfbc5ec7d7358
Natan <natanlaraferreira@gmail.com>
2020-05-05 10:56:43 -0300

    Testando comparacao

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {Sort} from '@angular/material/sort';
 
-export interface Dessert {
-  calories: number;
-  carbs: number;
-  fat: number;
-  name: string;
-  protein: number;
+export interface Node {
+  clienteNome: string,
+  assuntoNome: string,
+  modalidade: string,
+  faturador: string,
+  total: number 
 }
 
 @Component({
@@ -16,22 +16,22 @@ export interface Dessert {
 })
 export class AboutComponent {
 
-  desserts: Dessert[] = [
-    {name: 'Frozen yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
-    {name: 'Ice cream sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
-    {name: 'Eclair', calories: 262, fat: 16, carbs: 24, protein: 6},
-    {name: 'Cupcake', calories: 305, fat: 4, carbs: 67, protein: 4},
-    {name: 'Gingerbread', calories: 356, fat: 16, carbs: 49, protein: 4},
-  ];
+  node: Node[] = [
+    { clienteNome: "Lojas Americanas", assuntoNome:  "Conselhos de Sócios", modalidade: "FF", faturador: "TAB", total: 2000},
+    { clienteNome: "Olympique", assuntoNome:  "IBA 2019", modalidade: "FF", faturador: "SRRJ15", total: 3000},
+    { clienteNome: "Aniela Jordan", assuntoNome:  "Tributária", modalidade: "FF", faturador: "ABC", total: 5000},
+    { clienteNome: "Banco Santander", assuntoNome:  "Relcamações Especiais", modalidade: "PF", faturador: "ANI", total: 1000},
+    { clienteNome: "Ponto Frio", assuntoNome:  "Narrativa mock", modalidade: "FF", faturador: "ABC", total: 2000}
+  ]
 
-  sortedData: Dessert[];
+  sortedData: Node[];
 
   constructor() {
-    this.sortedData = this.desserts.slice();
+    this.sortedData = this.node.slice();
   }
 
   sortData(sort: Sort) {
-    const data = this.desserts.slice();
+    const data = this.node.slice();
     if (!sort.active || sort.direction === '') {
       this.sortedData = data;
       return;
@@ -40,11 +40,11 @@ export class AboutComponent {
     this.sortedData = data.sort((a, b) => {
       const isAsc = sort.direction === 'asc';
       switch (sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'calories': return compare(a.calories, b.calories, isAsc);
-        case 'fat': return compare(a.fat, b.fat, isAsc);
-        case 'carbs': return compare(a.carbs, b.carbs, isAsc);
-        case 'protein': return compare(a.protein, b.protein, isAsc);
+        case 'clienteNome': return compare(a.clienteNome, b.clienteNome, isAsc);
+        case 'assuntoNome': return compare(a.assuntoNome, b.assuntoNome, isAsc);
+        case 'modalidade': return compare(a.modalidade, b.modalidade, isAsc);
+        case 'faturador': return compare(a.faturador, b.faturador, isAsc);
+        case 'total': return compare(a.total, b.total, isAsc);
         default: return 0;
       }
     });
